3d57a9f1a7d3762e97e88ad9b7db7d8201b9e062
ileneslimen <slimennorhen1995@gmail.com>
2021-06-10 17:16:03 +0100

    ws js

---
+++ index.js
@@ -0,0 +1,107 @@
+// // types 
+// var x='543'
+// console.log(typeof(x))
+// var y=null
+// console.log(typeof(y))
+// var z
+// console.log(typeof(z))
+// var b=false
+// console.log(typeof(b))
+// var arr =[1,2,6,7]
+// console.log(typeof(arr))
+// var user ={name:'ali',age:6,true:true}
+// console.log(typeof(user))
+
+// mathematics operators
+// var a=5
+// var b=6
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// var i=1
+// i--
+// console.log(i)
+
+
+// // comparaison 
+// var a=5
+// var b=4
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a>=b)
+// console.log(a!=b)
+// console.log(a!==b)
+
+
+// Logical operator 
+// var a=0
+// var b=4
+// var z=3
+// console.log(a>b && b>z)
+// console.log(b>a || b<z)
+
+// selection if 
+// function A (n){
+//     if (n>0){
+//       return 'positif'
+//     }
+//     else if (n==0) {
+//       return 'zero'
+//     }
+//     else
+//      return 'negatif'
+//     }
+    
+//     console.log(A(-5))
+
+// switch
+// var n=5
+// switch(n){
+
+// case 0 : console.log('zero');
+// break;
+// case 1 :console.log('positif')
+// break;
+// case -1: console.log('negatif')
+// break;
+// default : console.log('not a number ');
+// break;
+
+// }
+
+// boucle for 
+// let arr =[5,7,9,8]
+
+// for (var x in arr ){
+//   console.log(x)
+// }
+// for (var x of arr ){
+//   console.log(x)
+// }
+//object
+// var user ={name:'ali', age:26}
+
+// for (var  key in user ){
+//   console.log(key)
+// }
+
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
+
+// min max
+// function minMax(arr) {
+// 	var min =arr[0]
+//   var max =arr[0]
+//   for (var i=0; i<arr.length;i++){
+// if (arr[i]<min)
+// min=arr[i]
+  
+// if (arr[i]>max)
+// max=arr[i]}
+// return [min,max]
+// }
+// console.log(minMax([1,2,8,9,0]))
